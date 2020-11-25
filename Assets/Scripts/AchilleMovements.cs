780f7211618cc20f0abdae03ed92e319a36d6b3c
Achille PACE <achille.pace@laposte.net>
2020-11-25 13:00:08 +0100

    AchilleMovements
    Uncontrolable

--- Assets/Scripts/AchilleMovements.cs
+++ Assets/Scripts/AchilleMovements.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class AchilleMovements : MonoBehaviour
 {
+    [SerializeField]
+    private float m_RotationForce = 5.0f;
+    [SerializeField]
+    private float m_PropulsionForce = 5.0f;
     private Rigidbody m_SpineRigidBody = null;
 
+
+    //Awake is called before anything else
+    private void Awake()
+    {
+        m_SpineRigidBody = GetComponent<Rigidbody>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -18,20 +28,25 @@ public class AchilleMovements : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Z))
         {
-            
+            m_SpineRigidBody.AddRelativeTorque(new Vector3(1.0f, 0.0f, 0.0f) * m_RotationForce);
         }
         if (Input.GetKey(KeyCode.S))
         {
-
+            m_SpineRigidBody.AddRelativeTorque(new Vector3(-1.0f, 0.0f, 0.0f) * m_RotationForce);
         }
 
         if (Input.GetKey(KeyCode.Q))
         {
-
+            m_SpineRigidBody.AddRelativeTorque(new Vector3(0.0f, -1.0f, 0.0f) * m_RotationForce);
         }
         if (Input.GetKey(KeyCode.D))
         {
+            m_SpineRigidBody.AddRelativeTorque(new Vector3(0.0f, 1.0f, 0.0f) * m_RotationForce);
+        }
 
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_SpineRigidBody.AddRelativeForce(new Vector3(0.0f, 0.0f, 1.0f) * m_PropulsionForce);
         }
     }
 }
