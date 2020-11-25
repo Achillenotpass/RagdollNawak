8827dd4da99d3211d577b18718014837c6dd6e1e
Achille PACE <achille.pace@laposte.net>
2020-11-25 11:21:51 +0100

    Tests on torque
    It works ! \o/

---
+++ Assets/Tests/TestTorque.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTorque : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.T))
+        {
+            GetComponent<Rigidbody>().AddTorque(new Vector3(-100.0f, 0.0f, 0.0f));
+        }
+    }
+}
