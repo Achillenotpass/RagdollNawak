9bc3341e82b0d8078ce3959e2933364fdf4a9c04
And4Code <Andrea.bois@e-artsup.net>
2020-11-25 18:25:10 +0100

    core loop et debut ui in game

---
+++ Assets/Scripts/CatchArea.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchArea : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> astronauts = new List<GameObject>();
+
+    public int astronautsNumber;
+
+    private void Update()
+    {
+
+        astronautsNumber = astronauts.Count;
+
+        if (astronauts.Count == 0)
+        {
+            Debug.Log("gagné");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        GameObject collidedObject = other.gameObject;
+
+        foreach (GameObject astronaut in astronauts)
+        {
+            if (collidedObject == astronaut)
+            {
+                astronauts.Remove(astronaut);
+                Destroy(collidedObject);
+               
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/InGameUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_AstroNumber;
+
+    private CatchArea m_CatchArea;
+
+    private void Start()
+    {
+        m_CatchArea = FindObjectOfType<CatchArea>();
+    }
+
+    private void Update()
+    {
+        m_AstroNumber.text =" x " +m_CatchArea.astronautsNumber;
+    }
+}
