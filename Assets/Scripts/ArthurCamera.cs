69c4c129c529b86c3868c62e053a625a03cf6cdf
ArthurCraste <acraste@gmail.com>
2020-11-25 15:37:11 +0100

    Controler Camera

---
+++ Assets/Scripts/ArthurCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArthurCamera : MonoBehaviour
+{
+    Transform player;
+
+    public Vector3 offset = new Vector3(0, -5, 10);
+
+    public bool useOffset;
+
+    public float rotateSpeed = 1;
+
+    void Start()
+    {
+        player = FindObjectOfType<ArthurMovement>().transform;
+
+        if (!useOffset)
+        {
+            offset = player.position - transform.position;
+        }
+
+        // Camoufler la souris
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        player.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        player.Rotate(-vertical, 0, 0);
+
+        float desiredYAngle = player.eulerAngles.y;
+        float desiredXAngle = player.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = player.position - (rotation * offset);
+
+        transform.LookAt(player);
+    }
+}
---
+++ Assets/Scripts/ArthurMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArthurMovement : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    Rigidbody rbPlayer;
+
+    Vector3 moveDirection;
+
+
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical") * moveSpeed) + (transform.right * Input.GetAxisRaw("Horizontal") * moveSpeed);
+
+        moveDirection = moveDirection.normalized * moveSpeed;
+
+        rbPlayer.velocity = moveDirection * Time.deltaTime;
+    }
+}
